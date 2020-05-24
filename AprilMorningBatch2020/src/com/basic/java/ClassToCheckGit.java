7b2fbe1373569851c4bf5c91e1008272bcec9587
Harshal.deshpande2 <Harshal.deshpande2@gmail.com>
2020-05-25 01:17:11 +0530

    x and y variables

--- AprilMorningBatch2020/src/com/basic/java/ClassToCheckGit.java
+++ AprilMorningBatch2020/src/com/basic/java/ClassToCheckGit.java
@@ -4,6 +4,8 @@ public class ClassToCheckGit {
 
 	public static void main(String[] args) {
 		
+		int x=10;
+		int y=20;
 		
 		System.out.println("Check Commit");
 		System.out.println("second commit");
