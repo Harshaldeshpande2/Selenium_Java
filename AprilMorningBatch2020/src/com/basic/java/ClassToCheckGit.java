72d7dc245435f22d38c374fa635d75f55f9c2d12
Harshal.deshpande2 <Harshal.deshpande2@gmail.com>
2020-05-30 07:08:58 +0530

    new classes added - 30 may
    adding new classes

--- AprilMorningBatch2020/src/com/basic/java/ClassToCheckGit.java
+++ AprilMorningBatch2020/src/com/basic/java/ClassToCheckGit.java
@@ -3,13 +3,12 @@ package com.basic.java;
 public class ClassToCheckGit {
 
 	public static void main(String[] args) {
-		
-		int x=10;
-		int y=20;
-		
+
+		int x = 10;
+		int y = 20;
+
 		System.out.println("Check Commit");
 		System.out.println("second commit");
-		
 
 	}
 
